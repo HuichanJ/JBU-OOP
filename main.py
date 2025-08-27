57fc42a337f22be98c228e5a6e5cb2854d25f742
HuichanJ <huichanlucasjung@gmail.com>
2025-08-27 09:49:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+EmpName = input("Type the name of the employee: ")
+Wages = int(input("What is your input? :"))
+Hours = int(input("How many hours do you work in a day? :"))
+Days = int(input("How many total days do you work? :"))
+
+Salary = Wages * Hours * Days
+print("Your total Salary is ", Salary)
