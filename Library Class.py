d8b648a6bf1d04b301151026c09c72d3eef274f4
HuichanJ <huichanlucasjung@gmail.com>
2025-10-10 08:49:55 -0500

    Add files via upload

---
+++ Library Class.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self):
+        self.BookID = ""
+        self.BookName = ""
+        self.Title = ""
+        self.Author = ""
+    def addBooks(self):
+
+class User
+    def __init__(self):
+        self.userID = ""
+        self.userName = ""
+        self.bookBorrowed = []
